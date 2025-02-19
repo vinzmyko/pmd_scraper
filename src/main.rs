2c9ffa3651e3dfc1f8c63bbd16e8471444fb45b7
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-02-19 22:49:51 +0000

    feat(rom): add game title extraction from ROM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+
+struct Rom {
+    path: PathBuf
+}
+
+impl Rom {
+    pub fn new<P: Into<PathBuf>>(path: P) -> Self {
+        Rom {
+            path: path.into()
+        }
+    }
+
+    pub fn read_game_title(&self) -> String {
+        let mut file = File::open(&self.path).unwrap();
+        let mut buffer = [0u8; 12];
+        
+        file.seek(SeekFrom::Start(0x000)).unwrap();
+        file.read_exact(&mut buffer).unwrap();
+        
+        String::from_utf8_lossy(&buffer)
+            .trim_end_matches('\0')
+            .to_string()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rom_eu = Rom::new("../../ROMs/pmd_eos_eu.nds");
+    println!("Game Title: {}", rom_eu.read_game_title());
 }
