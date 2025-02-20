e0580ab45cc92e653f2b775a3b792fa2579e1548
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-02-20 11:14:01 +0000

    feat(rom-header): add game header extraction from ROM

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,63 @@ impl Rom {
             path: path.into()
         }
     }
+}
 
-    pub fn read_game_title(&self) -> String {
-        let mut file = File::open(&self.path).unwrap();
-        let mut buffer = [0u8; 12];
-        
-        file.seek(SeekFrom::Start(0x000)).unwrap();
-        file.read_exact(&mut buffer).unwrap();
-        
-        String::from_utf8_lossy(&buffer)
+#[derive(Debug)]
+#[allow(dead_code)]
+struct RomHeader {
+    game_title: String,
+    gamecode: String,
+    makercode: String,
+    unitcode: u8,
+    nds_region: u8,
+    rom_version: u8
+}
+
+fn read_header(rom_path: &PathBuf) -> RomHeader {
+    let mut file = File::open(rom_path).unwrap();
+    
+    // Read game title (0x000, 12 bytes)
+    let mut title_buffer = [0u8; 12];
+    file.seek(SeekFrom::Start(0x000)).unwrap();
+    file.read_exact(&mut title_buffer).unwrap();
+    
+    // Read gamecode (0x00C, 4 bytes)
+    let mut gamecode_buffer = [0u8; 4];
+    file.seek(SeekFrom::Start(0x00C)).unwrap();
+    file.read_exact(&mut gamecode_buffer).unwrap();
+    
+    // Read makercode (0x010, 2 bytes)
+    let mut makercode_buffer = [0u8; 2];
+    file.seek(SeekFrom::Start(0x010)).unwrap();
+    file.read_exact(&mut makercode_buffer).unwrap();
+    
+    // Read single bytes
+    let mut unitcode = [0u8; 1];
+    file.seek(SeekFrom::Start(0x012)).unwrap();
+    file.read_exact(&mut unitcode).unwrap();
+
+    let mut region = [0u8; 1];
+    file.seek(SeekFrom::Start(0x01D)).unwrap();
+    file.read_exact(&mut region).unwrap();
+
+    let mut version = [0u8; 1];
+    file.seek(SeekFrom::Start(0x01E)).unwrap();
+    file.read_exact(&mut version).unwrap();
+
+    RomHeader {
+        game_title: String::from_utf8_lossy(&title_buffer)
             .trim_end_matches('\0')
-            .to_string()
+            .to_string(),
+        gamecode: String::from_utf8_lossy(&gamecode_buffer).to_string(),
+        makercode: String::from_utf8_lossy(&makercode_buffer).to_string(),
+        unitcode: unitcode[0],
+        nds_region: region[0],
+        rom_version: version[0],
     }
 }
 
 fn main() {
-    let rom_eu = Rom::new("../../ROMs/pmd_eos_eu.nds");
-    println!("Game Title: {}", rom_eu.read_game_title());
+    let rom_eu = Rom::new("../../ROMs/pmd_eos_us.nds");
+    println!("{:#?}", read_header(&rom_eu.path));
 }
